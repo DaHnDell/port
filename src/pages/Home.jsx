52fefb01390981d61844792db3d7df0dc4663be2
DaHnDell <khanman1208@gmail.com>
2025-04-10 16:20:47 +0900

    page jsx add

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+      <main style={{ marginTop: '120px', padding: '2rem' }}>
+        <h2>Home Page</h2>
+        <p>This is the Home page for DahnDell's Portfolio</p>
+      </main>
+    );
+  };
+  
+  export default Home;
+  
